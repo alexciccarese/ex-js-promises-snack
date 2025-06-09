02961e49f5e453da2d7b318dabc5a78588597a66
alexciccarese <alex.ciccarese@gmail.com>
2025-06-09 14:59:16 +0200

    Implement getPostTitle function to fetch and return post title using Promise

--- 01/script.js
+++ 01/script.js
@@ -3,3 +3,18 @@
 
 /*  🎯 Bonus: Ottieni l'intero post con l'autore
 Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}. */
+
+function getPostTitle(id) {
+
+  return new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+    .then(res => res.json())
+    .then(post => resolve(post.title))
+    .catch(reject)
+
+})
+}
+
+getPostTitle(1)
+.then(title => console.log(`Titolo del post:`, title))
+.catch(error => console.error(error))
